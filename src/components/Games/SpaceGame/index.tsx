5175f856aa0b1b0a039e43575686fef6b065fb67
David Plell <plelldavid@gmail.com>
2023-09-19 14:20:13 -0700

    clouds

--- src/components/Games/SpaceGame/index.tsx
+++ src/components/Games/SpaceGame/index.tsx
@@ -6,28 +6,61 @@ import { gamePositions } from "../../../Stores/constants";
 import { Lights } from "../../Lights";
 import { Spaceship } from "./Objects/Spaceship";
 import { Enemy } from "./Objects/Enemy";
+import { useFrame } from "@react-three/fiber";
 
 export const SpaceGame = () => {
   const ref = useRef<Group | null>(null);
+  const cloudRef = useRef<Group | null>(null);
+  const cloudRef2 = useRef<Group | null>(null);
 
   const game = useGame((s) => s.game);
+
+  useFrame(() => {
+    if (cloudRef.current) {
+      cloudRef.current.position.y -= 0.1;
+
+      if (cloudRef.current.position.y < -25) {
+        cloudRef.current.position.y = 25;
+      }
+    }
+
+    if (cloudRef2.current) {
+      cloudRef2.current.position.y -= 0.1;
+
+      if (cloudRef2.current.position.y < -25) {
+        cloudRef2.current.position.y = 25;
+      }
+    }
+  });
 
   return (
     <group ref={ref} position={gamePositions[game].gamePosition}>
       <Lights />
-      <Stars />
-      <Cloud
-        opacity={0.5}
-        speed={0.4} // Rotation speed
-        width={20} // Width of the full cloud
-        depth={1.5} // Z-dir depth
-        segments={20} // Number of particles
-      />
+
+      <group ref={cloudRef}>
+        <Cloud
+          opacity={0.5}
+          speed={0.4} // Rotation speed
+          width={20} // Width of the full cloud
+          depth={1.5} // Z-dir depth
+          segments={20} // Number of particles
+        />
+      </group>
+
+      <group ref={cloudRef2} position-y={25}>
+        <Cloud
+          opacity={0.7}
+          speed={0.7} // Rotation speed
+          width={30} // Width of the full cloud
+          depth={1} // Z-dir depth
+          segments={20} // Number of particles
+        />
+      </group>
       <Spaceship position={[0, -8, 0]} />
       <Enemy position={[0, 5, 0]} />
       <mesh receiveShadow position-z={-10}>
-        <planeGeometry args={[26.8, 26]} />
-        <meshStandardMaterial color={"#569f3e"} />
+        <planeGeometry args={[45, 40]} />
+        <meshStandardMaterial color={"skyblue"} />
       </mesh>
     </group>
   );
