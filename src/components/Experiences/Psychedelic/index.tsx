ade8c9a1e5193623ec4b669b57f27d951b173816
David Plell <plelldavid@gmail.com>
2023-11-09 11:00:22 -0800

    game

--- src/components/Experiences/Psychedelic/index.tsx
+++ src/components/Experiences/Psychedelic/index.tsx
@@ -17,6 +17,7 @@ import { Shader } from "./Shader";
 import {
   CircleGeometry,
   Color,
+  Group,
   MathUtils,
   Mesh,
   MeshBasicMaterial,
@@ -30,57 +31,32 @@ import { Projectile } from "../../../Stores/types";
 import { useObjectIntersectsManyB } from "../hooks/useObjectsIntersect";
 import { useFrame } from "@react-three/fiber";
 import { useAudioAnalysis } from "../hooks/useAudioAnalysis";
+import { useControls } from "leva";
+import { GameProgress } from "../common/GameProgress";
 
 const triangleSize = 12.8;
 const triangleYAdjust = 1.7;
 
 const buffer = 60;
 
-const bulletSpeed = 0.3;
+const bulletSpeed = 0.5;
 
-const triangles = [
+type Triangle = {
+  x: number;
+  y: number;
+  rotation: number;
+  projectilePosition: Vector3;
+  projectileDirection: Vector3;
+  order: number;
+};
+const triangles: Triangle[] = [
   {
     x: triangleSize,
     y: 0,
     rotation: Math.PI,
     projectilePosition: new Vector3(buffer, 0, 0),
     projectileDirection: new Vector3(-bulletSpeed, 0, 0),
-  },
-  {
-    x: -triangleSize,
-    y: 0,
-    rotation: Math.PI * 2,
-    projectilePosition: new Vector3(-buffer, 0, 0),
-    projectileDirection: new Vector3(bulletSpeed, 0, 0),
-  },
-  {
-    x: -triangleSize * 0.5,
-    y: -triangleSize + triangleYAdjust,
-    rotation: Math.PI * 3,
-    projectilePosition: new Vector3(-buffer * 0.45, -buffer * 0.8, 0),
-    projectileDirection: new Vector3(bulletSpeed * 0.35, bulletSpeed * 0.65, 0),
-  },
-  {
-    x: triangleSize * 0.5,
-    y: triangleSize - triangleYAdjust,
-    rotation: Math.PI * 4,
-    projectilePosition: new Vector3(buffer * 0.45, buffer * 0.8, 0),
-    projectileDirection: new Vector3(
-      -bulletSpeed * 0.35,
-      -bulletSpeed * 0.65,
-      0
-    ),
-  },
-  {
-    x: -triangleSize * 0.5,
-    y: triangleSize - triangleYAdjust,
-    rotation: Math.PI * 5,
-    projectilePosition: new Vector3(-buffer * 0.45, buffer * 0.8, 0),
-    projectileDirection: new Vector3(
-      bulletSpeed * 0.35,
-      -bulletSpeed * 0.65,
-      0
-    ),
+    order: 0,
   },
   {
     x: triangleSize * 0.5,
@@ -92,6 +68,47 @@ const triangles = [
       bulletSpeed * 0.65,
       0
     ),
+    order: 1,
+  },
+  {
+    x: -triangleSize * 0.5,
+    y: -triangleSize + triangleYAdjust,
+    rotation: Math.PI * 3,
+    projectilePosition: new Vector3(-buffer * 0.45, -buffer * 0.8, 0),
+    projectileDirection: new Vector3(bulletSpeed * 0.35, bulletSpeed * 0.65, 0),
+    order: 2,
+  },
+  {
+    x: -triangleSize,
+    y: 0,
+    rotation: Math.PI * 2,
+    projectilePosition: new Vector3(-buffer, 0, 0),
+    projectileDirection: new Vector3(bulletSpeed, 0, 0),
+    order: 3,
+  },
+  {
+    x: -triangleSize * 0.5,
+    y: triangleSize - triangleYAdjust,
+    rotation: Math.PI * 5,
+    projectilePosition: new Vector3(-buffer * 0.45, buffer * 0.8, 0),
+    projectileDirection: new Vector3(
+      bulletSpeed * 0.35,
+      -bulletSpeed * 0.65,
+      0
+    ),
+    order: 4,
+  },
+  {
+    x: triangleSize * 0.5,
+    y: triangleSize - triangleYAdjust,
+    rotation: Math.PI * 4,
+    projectilePosition: new Vector3(buffer * 0.45, buffer * 0.8, 0),
+    projectileDirection: new Vector3(
+      -bulletSpeed * 0.35,
+      -bulletSpeed * 0.65,
+      0
+    ),
+    order: 5,
   },
 ];
 
@@ -105,22 +122,20 @@ export const Psychedelic = () => {
   );
 };
 
-// const circleGeo = new CircleGeometry(triangleSize, 0, hovered?.rotation);
-
 const Content = () => {
-  const [hovered, setHovered] = useState(null);
+  const [hovered, setHovered] = useState<Triangle | null>(null);
   const sensorRef = useRef<Mesh | null>(null);
   const beatMeshRef = useRef<Mesh | null>(null);
 
   return (
     <Suspense fallback={null}>
       <group position-z={15}>
-        <Beats sensorRef={sensorRef} />
+        <Beats sensorRef={sensorRef} hovered={hovered} />
         <PsychCone setHovered={setHovered} />
 
         <mesh
           ref={beatMeshRef}
-          visible={!!hovered}
+          visible={false} //!!hovered}
           position-x={hovered?.x}
           position-y={hovered?.y}
           rotation-z={hovered?.rotation}
@@ -154,12 +169,128 @@ const Content = () => {
 
 type BeatsProps = {
   sensorRef: MutableRefObject<Mesh | null>;
+  hovered: any;
 };
 
-const Beats = ({ sensorRef }: BeatsProps) => {
-  const projectilesRef = useRef<Refs>({});
+const lime = new Color("white");
+const white = new Color("hotpink");
 
-  return <Projectiles sensorRef={sensorRef} refs={projectilesRef} />;
+const Beats = ({ sensorRef, hovered }: BeatsProps) => {
+  const projectilesRef = useRef<Refs>({});
+  const barrierGroupRef = useRef<Group | null>(null);
+  const barrierRef = useRef<Mesh | null>(null);
+  const barrierMaterialRef = useRef<MeshStandardMaterial | null>(null);
+  const [score, setScore] = useState(0);
+  const [barrierColor, setBarrierColor] = useState(white);
+  const [barrierIntensity, setBarrierIntensity] = useState(1.1);
+  const [level, setLevel] = useState(1);
+  const [barrierRotation, setBarrierRotation] = useState({
+    rotation: 0,
+    order: 0,
+  });
+
+  useEffect(() => {
+    setBarrierIntensity(2);
+    setBarrierColor(lime);
+    const timeout = setTimeout(() => {
+      setBarrierColor(white);
+      setBarrierIntensity(1.1);
+    }, 100);
+    return () => {
+      if (timeout) clearTimeout(timeout);
+    };
+  }, [score]);
+
+  useEffect(() => {
+    if (hovered) {
+      const turn = (Math.PI * 2) / 6;
+      const direction = barrierRotation.order - hovered.order;
+      let rotation = barrierRotation.rotation + turn * direction;
+
+      if (
+        barrierRotation?.order === 0 &&
+        hovered.order === triangles.length - 1
+      ) {
+        rotation = barrierRotation.rotation + turn;
+      } else if (
+        hovered.order === 0 &&
+        barrierRotation.order === triangles.length - 1
+      ) {
+        rotation = barrierRotation.rotation - turn;
+      }
+
+      setBarrierRotation({
+        order: hovered.order,
+        rotation,
+      });
+    }
+  }, [hovered]);
+
+  const intersect = useObjectIntersectsManyB(barrierRef, projectilesRef);
+
+  useFrame(() => {
+    if (barrierGroupRef.current) {
+      barrierGroupRef.current.rotation.z = MathUtils.lerp(
+        barrierGroupRef.current.rotation.z,
+        barrierRotation.rotation,
+        0.2
+      );
+    }
+
+    if (barrierMaterialRef.current) {
+      barrierMaterialRef.current.emissiveIntensity = MathUtils.lerp(
+        barrierMaterialRef.current.emissiveIntensity,
+        barrierIntensity,
+        0.3
+      );
+    }
+
+    if (intersect.current.length > 0) {
+      projectilesRef.current[intersect.current[0]].position.set(
+        0,
+        DEAD_ZONE_Y + 100,
+        0
+      );
+
+      postDebounce(
+        "hit",
+        () => {
+          setScore(score + 1);
+        },
+        200
+      );
+    }
+  });
+
+  return (
+    <>
+      <GameProgress
+        position={new Vector3(0, 11, 110)}
+        type='points'
+        max={5}
+        score={score}
+        level={level}
+        levelPrefix='LEVEL '
+        setLevel={() => {
+          setLevel(level + 1);
+          setScore(0);
+        }}
+        scale={0.1}
+      />
+
+      <group ref={barrierGroupRef}>
+        <mesh ref={barrierRef} position={[26, 0, 0]}>
+          <boxGeometry args={[0.3, 25, 30, 10, 10]} />
+          <meshStandardMaterial
+            emissive='white'
+            wireframe
+            ref={barrierMaterialRef}
+          />
+        </mesh>
+      </group>
+      <Projectiles sensorRef={sensorRef} refs={projectilesRef} />
+    </>
+  );
 };
 
 type Props = {
@@ -180,6 +311,8 @@ export const Projectiles = ({ refs, sensorRef }: Props) => {
     {}
   );
 
+  const orbGroupRef = useRef<Group | null>(null);
+
   const [selectedPointId, setSelectedPointId] = useState(0);
 
   const intersect = useObjectIntersectsManyB(sensorRef, refs);
@@ -187,7 +320,7 @@ export const Projectiles = ({ refs, sensorRef }: Props) => {
   const { data, update, getLowAvg, getHighAvg } =
     useAudioAnalysis("audio/beat.mp3");
 
-  useFrame(({ camera }) => {
+  useFrame(({ camera }, delta) => {
     const lowAvg = getLowAvg();
 
     camera.fov = MathUtils.lerp(camera.fov, 50 - lowAvg / 105, 0.2);
@@ -206,10 +339,14 @@ export const Projectiles = ({ refs, sensorRef }: Props) => {
         1000
       );
     }
+
+    if (orbGroupRef.current) {
+      // orbGroupRef.current.rotation.z += 1 * delta;
+    }
   });
 
   const initNewProjectile = () => {
-    const index = selectedPointId + 1 > triangles.length ? 0 : selectedPointId; //Math.floor(Math.random() * triangles.length);
+    const index = Math.floor(Math.random() * triangles.length);
     const tri = triangles[index];
 
     setSelectedPointId(index + 1);
@@ -242,7 +379,7 @@ export const Projectiles = ({ refs, sensorRef }: Props) => {
   };
 
   return (
-    <group>
+    <group ref={orbGroupRef}>
       {Object.keys(projectiles).map((p) => {
         const self = projectiles[p];
         if (self) {
@@ -282,7 +419,7 @@ type OrbProps = {
 
 const orbVec = new Vector3();
 
-const white = new Color("mediumpurple");
+const mediumpurple = new Color("mediumpurple");
 const hotpink = new Color("hotpink");
 
 const Orb = ({ position, id, selectedPointId }: OrbProps) => {
@@ -294,7 +431,7 @@ const Orb = ({ position, id, selectedPointId }: OrbProps) => {
     if (ref.current) {
       if (selected) {
         ref.current.scale.lerp(orbVec.set(2, 2, 2), 0.1);
-        materialRef.current?.emissive.lerp(white, 0.2);
+        materialRef.current?.emissive.lerp(mediumpurple, 0.2);
       } else {
         ref.current.scale.lerp(orbVec.set(1, 1, 1), 0.1);
         materialRef.current?.emissive.lerp(hotpink, 0.2);
